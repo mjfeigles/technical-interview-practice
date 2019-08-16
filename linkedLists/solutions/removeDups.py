58673dc755830de833e182e44cc6d0893c6c7cb8
mjfeigles <mjfeigles@gmail.com>
2019-08-16 16:21:41 -0400

    remove dups passes tests

--- linkedLists/solutions/removeDups.py
+++ linkedLists/solutions/removeDups.py
@@ -5,31 +5,40 @@ output: the same unsorted linked list without duplicates
 Run instructions:
 python3 remove_dups.py head
 Note: head is the head node
-
+"""
 import sys
 
 class Node:
     def __init__(self, value):
         self.next = None
         self.value = value
-"""
+
 def remove_dups(head):
     if head is None or head.next is None:
         return head
-
     map = {}
-
     current = head
-
-    while current.next != None:
+    map[current.value] = 1
+    while current != None and current.next != None:
         if current.next.value in map:
             current.next = current.next.next
+            continue
         else:
             map[current.next.value] = 1
         current = current.next
 
+    print(map.keys())
+
     return head
 
-def foo():
-    print("bonjour")
-    return 5
+def print_linked_list(head):
+    if head is None:
+        return "empty list"
+    printed_list = ""
+    while head is not None:
+        printed_list = printed_list + str(head.value) + "->"
+        head = head.next
+    printed_list = printed_list[:len(printed_list)-2]
+    return printed_list
+
+
--- linkedLists/tests/test_removeDups.py
+++ linkedLists/tests/test_removeDups.py
@@ -1,10 +1,89 @@
+"""
+Run instructions (for all tests from top directory):
+python3 -m unittest discover -s ./linkedLists/tests
+
+Tests solutions to linkedList problems--> prints output for visual check
+"""
+
 import unittest
 from linkedLists.solutions import removeDups
+import random
 
 class TestRemoveDups(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual(removeDups.foo(), 6)
+    def test_noDupsOrdered(self):
+        head = removeDups.Node(1)
+        current = head
+        for i in range(5):
+            current.next = removeDups.Node(i+2)
+            current = current.next
+        expected = head
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+        self.assertEqual(expected, head)
+
+    def test_emptyList(self):
+        head = None
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+        self.assertEqual(None, head)
+
+    def test_dupTail(self):
+        head = removeDups.Node(0)
+        current = head
+        for i in range(5):
+            current.next = removeDups.Node(i+1)
+            current = current.next
+        current.next = removeDups.Node(2)
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+        # self.assertEqual(solution, head)
+
+    def test_dupHead(self):
+        head = removeDups.Node(0)
+        current = head
+        for i in range(5):
+            current.next = removeDups.Node(i)
+            current = current.next
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+        # self.assertEqual(solution, head)
+
+    def test_dupUnordered(self):
+        head = removeDups.Node(0)
+        current = head
+        for i in range(10):
+            current.next = removeDups.Node(random.randint(0, 10)) # int in range [0, 10]
+            current = current.next
+
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+        # self.assertEqual(solution, head)
+
+    def test_allRepeats(self):
+        head = removeDups.Node(6)
+        current = head
+        for i in range(10):
+            current.next = removeDups.Node(6)
+            current = current.next
+        print()
+        print("in: " + '\t' + removeDups.print_linked_list(head))
+        removeDups.remove_dups(head)
+        print("out: " + '\t' + removeDups.print_linked_list(head))
+
+
 
 if __name__ == "__main__":
+    # suite = unittest.TestLoader().loadTestsFromTestCase(TestRemoveDups)
+    # testResult = unittest.TextTestRunner(verbosity=2).run(suite)
     unittest.main()
 
